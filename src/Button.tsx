e431186660daabbbefb30a762f97a4cf87b79bfd
Vaishali Dhanoa <vaishali.dhanoa@gmail.com>
2021-03-09 11:37:26 +0100

    Intermediate commit

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Report } from 'powerbi-client';
+import { Report, VisualDescriptor } from 'powerbi-client';
 import 'powerbi-report-authoring';
 
 interface ButtonProps {myReport: any | Report};
@@ -14,7 +14,7 @@ class Button extends React.Component<ButtonProps, {}>
       return (
           <div>
               <button onClick={this.getVisInfo.bind(this)}>Get Marks of Visual 1</button>
-              <button onClick={this.getMarkInfo.bind(this)}> New butoon</button>
+              <button onClick={this.getMarkInfo.bind(this)}> New Button</button>
           </div>
       );
     }
@@ -24,16 +24,24 @@ class Button extends React.Component<ButtonProps, {}>
         const report = this.props.myReport as Report;
         const pages = await report.getPages();
         const visuals = await pages[1].getVisuals();
-        const capability = await visuals[0].getCapabilities();
-        console.log('Capability', capability);
-        capability.dataRoles?.forEach((role) => {
-          // print the visual type
-          console.log(role.name, role.displayName);
-        })
-    }
+
+        for(const visual of visuals) {
+
+          console.log('Visual: ', visual.title , ' Type: ', visual.type, ' Data: ');
+
+          const capability = await visual.getCapabilities();
+          capability.dataRoles?.forEach((role) => {
+            // print the visual type
+            console.log(role.name, ' Display Name: ', role.displayName);
+          })
+
+          console.log('______________________________________________');
+        }
+      }
 
     getMarkInfo(): void{
       //do something here
+      console.log('Does nothing at the moment');
     }
 }
 
