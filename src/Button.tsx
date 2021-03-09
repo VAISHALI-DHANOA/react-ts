47e15c9dd983f882b58aaf686fbe30a104d4663f
Vaishali Dhanoa <vaishali.dhanoa@gmail.com>
2021-03-09 16:19:11 +0100

    check if visual has changed

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Report } from 'powerbi-client';
+import { Page, Report, VisualDescriptor } from 'powerbi-client';
 import 'powerbi-report-authoring';
+import * as models from "powerbi-models";
+import { render } from "react-dom";
+import { ExitStatus } from "typescript";
 
 interface ButtonProps {myReport: any | Report};
 
-class Button extends React.Component<ButtonProps, {}>
-{
+class Button extends React.Component<ButtonProps, {}> {
+
+    private renderedVis = new Array<string>();
+    private report: any | Report = null;
+    private pages: Page[] = [];
+    private visuals: VisualDescriptor[] = [];
+    private initialData: {type: string; result: models.IExportDataResult}[] = [];//{type: '', result: {data : ''}}[4];
+    private finalData: {type: string; result: models.IExportDataResult}[] = []; //{type: '', result: {data : ''}}[4];
+
+
     constructor(props: ButtonProps) {
         super(props);
     }
@@ -13,36 +24,69 @@ class Button extends React.Component<ButtonProps, {}>
     render(): JSX.Element {
       return (
           <div>
-              <button onClick={this.getVisInfo.bind(this)}>Get Marks of Visual 1</button>
-              <button onClick={this.getMarkInfo.bind(this)}> New Button</button>
+              <button onClick={this.getVisInfo.bind(this)}>Register Report Events</button>
+              <button onClick={this.getMarkInfo.bind(this)}> Get Changed Visuals</button>
           </div>
       );
     }
 
     async getVisInfo(): Promise<void> {
 
-        const report = this.props.myReport as Report;
-        report.on("dataSelected", function(event) {
-          console.log('Data Selected'); // , event.detail);
+       this.report = this.props.myReport as Report;
+       this.pages = await this.report.getPages();
+       this.visuals = await this.pages[1].getVisuals();
+
+       for( const visual of this.visuals) {
+
+        if(visual.type !== 'slicer') {
+          const result = await visual.exportData(models.ExportDataType.Summarized);
+          this.initialData.push({type: visual.type, result: result});
+        }
+      }
+
+        this.report.on("visualRendered", (event: any) => {
+          const vis = event.detail.name as string;
+          this.renderedVis.push(vis);
         })
 
-
-
-        report.on("visualRendered", function(event) {
-          console.log('Visual Rendered' , event);
-        })
+        /*
+        // report.on("dataSelected", function(event) {
+        //   console.log('Data Selected'); // , event.detail);
+        // })
 
         // if you change a visual this is called
-        report.on("selectionChanged", function(event) {
-          console.log("Selection changed ", event);
-        })
+        // report.on("selectionChanged", function(event) {
+        //   console.log("Selection changed ", event);
+        // })
+        */
+      }
 
-        const pages = await report.getPages();
-        const visuals = await pages[1].getVisuals();
+      async getMarkInfo(): Promise<void> {
 
-        for(const visual of visuals) {
+          this.finalData = [];
 
-          console.log('Visual: ', visual.type , ' Type: ', visual.name);
+          await this.checkVisChanged();
+
+          console.log('Initial Data --> ', this.initialData);
+          console.log('Final Data --> ', this.finalData);
+
+          // Compare intial and finalData
+          for(const inData of this.initialData) {
+            for(const fiData of this.finalData) {
+              if(inData.type === fiData.type) {
+                if(inData.result.data === fiData.result.data) {
+                  console.log('Data didnt change');
+                } else {
+                  console.log('Data definitely changed in type --> ', inData.type);
+                }
+              }
+            }
+          }
+      }
+
+      async checkVisChanged(): Promise<void> {
+
+        for(const visual of this.visuals) {
 
             // gets the data field on the y-axis
             // const field = await visual.getDataFields('Y');
@@ -60,21 +104,30 @@ class Button extends React.Component<ButtonProps, {}>
 
 
           // const capability = await visual.getCapabilities();
-          // console.log("Capability ", capability);
-
           // capability.dataRoles?.forEach((role) => {
           //   // print the visual type
           //   console.log(role.name, ' Display Name: ', role.displayName);
           // })
 
+
+          if(this.renderedVis.length) {
+
+            for(const vis of this.renderedVis) {
+
+              if(vis === visual.name && visual.type !== 'slicer') {
+                console.log('Type --> ', visual.type, ' --> ', visual.title, 'has changed' );
+                const result = await visual.exportData(models.ExportDataType.Summarized);
+                this.finalData.push({type: visual.type, result: result});
+              }
+            }
+
+          }
+
           console.log('______________________________________________');
         }
       }
 
-    getMarkInfo(): void{
-      //do something here
-      console.log('Does nothing at the moment');
-    }
+
 }
 
 export default Button;
